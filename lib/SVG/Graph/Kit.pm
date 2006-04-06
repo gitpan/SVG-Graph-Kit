5b88024f06aed4bafd5aea724925cb830ed1fa5f
Gene Boggs <gene@cpan.org>
2006-04-06 03:49:39 +0000

    Import of GENE/SVG-Graph-Kit-0.00_4 from CPAN.
    gitpan-cpan-distribution: SVG-Graph-Kit
    gitpan-cpan-version:      0.00_4
    gitpan-cpan-path:         GENE/SVG-Graph-Kit-0.00_4.tar.gz
    gitpan-cpan-author:       GENE
    gitpan-cpan-maturity:     developer

--- lib/SVG/Graph/Kit.pm
+++ lib/SVG/Graph/Kit.pm
@@ -1,7 +1,7 @@
-# $Id: Kit.pm,v 1.2 2006/03/30 04:38:48 gene Exp $
+# $Id: Kit.pm,v 1.5 2006/04/06 03:31:51 gene Exp $
 
 package SVG::Graph::Kit;
-$VERSION = '0.00_2';
+$VERSION = '0.00_4';
 use strict;
 use warnings;
 use Carp;
@@ -14,31 +14,80 @@ sub new {
     my( $class, %args ) = @_;
     my $proto = ref( $class ) || $class;
 
-    # Skim-off the glyph and data item layer.
-    my @items = @{ $args{items} };
-    delete $args{items};
+    # Strip-off the glyph and data item layers.
+    my %kit = ();
+    if( $args{_autoaxis} ) {
+        $kit{_autoaxis} = $args{_autoaxis};
+        delete $args{_autoaxis};
+        $args{width}  ||= 600;
+        $args{height} ||= 600;
+        $args{margin} ||= 35;
+    }
+    if( $args{_items} ) {
+        $kit{_items} = $args{_items};
+        delete $args{_items};
+    }
 
     # Construct the SVG::Graph object with the remaining arguments.
     my $self = $class->SUPER::new( %args );
     bless $self, $class;
 
     # Proceed to the automatic metaglyphification.
-    $self->_init( @items );
+    $self->_init( %kit );
 
     return $self;
 }
 
 sub _init {
     my $self = shift;
+    my %args = @_;
+
+    if( $args{_autoaxis} ) {
+        my $x = $args{_autoaxis}->{x};
+        my $y = $args{_autoaxis}->{y};
+        my $s = $args{_autoaxis}->{s} || 1;
+
+        # The scaled magnitude.
+        $x ||= (int $args{_autoaxis}->{m} / $args{_autoaxis}->{s}) || 1;
+        # Largest data-point.
+        $y ||= $args{_autoaxis}->{n} || 1;
+
+        my $labels = [ map { $_ * $x } 0 .. $y ];
+        my $grid = 'palegray';
+        my $line = { stroke => $grid, fill => $grid, 'fill-opacity' => 0.5, };
+
+        unshift @{ $args{_items} },
+            # Create an axis.
+            { axis => {
+                #x_intercept => 100, y_intercept => 100,
+                x_absolute_ticks => $x, y_absolute_ticks => $x,
+                x_tick_labels => $labels, y_tick_labels => $labels,
+                stroke => $grid,
+              },
+            },
+            # Create the reference lines.
+            { data => [ [ 0, 0 ], [ $y, $y ] ],
+              line => $line,
+            },
+            ( map { {
+                data => [ [ 0, $_ * $x ], [ $y, $_ * $x ] ],
+                line => $line,
+              } } 1 .. $y / $x ),
+            ( map { {
+                data => [ [ $_ * $x, 0 ], [ $_ * $x, $y ] ],
+                line => $line,
+              } } 1 .. $y / $x );
+    }
+
     # Give unto us a frame!
     my $master_frame = $self->add_frame();
     # Each item is a hashref with data or presentation info.
-    for my $item ( @_ ) {
+    for my $item ( @{ $args{_items} } ) {
         # I never do nuthin' wrong but I always get the blame.
         my $frame = $master_frame->add_frame();
         while( my( $key, $val ) = each %$item ) {
             if( $key eq 'data' ) {
-                # Make sure that the data is a proper dataset.
+                # Make sure that the data is a proper data set first.
                 $frame->add_data( dataset( $val ) );
             }
             else {
@@ -82,33 +131,23 @@ SVG::Graph::Kit - Simplified data plotting
 =head1 SYNOPSIS
 
   use SVG::Graph::Kit;
-  my @x = qw( 2 3 5 7 11 13 17 19 23 29 31 37 41 );
+  my @x = qw(2 3 5 7 11 13 17 19 23 29 31 37 41);
   my $i = 0;
-  while( (my $p = <>) =~ s/\D//g && ++$i <= $max ) {
-     push @x, $p;
-  }
-  $i = 0;
+  my $data = [ map { [ ++$i, $_ ] } @x ];
   my $g = SVG::Graph::Kit->new(
-     width => 600, height => 600, margin => 30,
-     items => [
-        { axis => { 'x_absolute_ticks' => 1, 'y_absolute_ticks' => 1,
-                    'stroke' => 'gray', },
-          data => [ [0,0], [ $x[-1], $x[-1] ] ],
-          line => { stroke => 'gray' },
-        },
-        { data => [ map { [ ++$i, $_ ] } @x ],
-          line => { stroke => 'yellow' },
-          scatter => { stroke => 'blue' },
-        },
-     ],
+    _autoaxis => { m => @x, n => $x[-1], s => @x, },
+    _items => [
+        { data => $data, line => { stroke => 'yellow'}, },
+        { data => $data, scatter => { stroke => 'blue' }, },
+    ],
   );
   print $g->draw;
 
 =head1 DESCRIPTION
 
-An C<SVG::Graph::Kit> object is a simplified, restricted, automatic
-data plotting tool that let's you plot data without needing to handle
-nested frames or coordinate mapping.
+An C<SVG::Graph::Kit> object is a simplified, automated tool that
+allows data plotting without requiring knowledge of the C<SVG::Graph>
+API.
 
 =head1 PUBLIC METHODS
 
@@ -116,15 +155,22 @@ nested frames or coordinate mapping.
 
   my $obj = SVG::Graph::Kit->new(%arguments);
 
-Return a new C<SVG::Graph::Kit> instance with any data or glyph
-B<item>s automatically added to the plot.
+Return a new C<SVG::Graph::Kit> instance with any given data or glyphs
+automatically added to the plot.
 
-The arguments can be any valid C<SVG::Graph> construction parameters,
-like width and height, plus an B<item> list of data and glyphs.  The
-glyphs are defined by C<SVG::Graph>.  The data can be a 1D list of
-numbers, an array reference of 1, 2 or 3-D data points, a hash
-reference with "x, y, z" keyed coordinates, a list of
-C<SVG::Graph::Data::Datum> points or a C<SVG::Graph::Data> object.
+This method can be called with any valid C<SVG::Graph> construction
+parameters (e.g. width, height, margin) plus an B<_items> list of
+things to show - data and glyphs.  The glyphs are defined by
+C<SVG::Graph> and the actual data can be either a 1-D list of numbers,
+an array reference of 1, 2 or 3-D data points, a hash reference with
+"x, y, z" keyed coordinates, a list of C<SVG::Graph::Data::Datum>
+points or a C<SVG::Graph::Data> object.
+
+If this method is invoked with the optional B<_autoaxis> parameter,
+an axis with scaled labels and grid-lines is automatically added to
+the graph.  This parameter should be a hash reference having keys
+B<m>, B<n> and B<s> that are used as the number of ticks plotted and
+the largest tick desired and the scaling factor, respectively.
 
 =head1 SEE ALSO
 
--- t/SVG-Graph-Kit.t
+++ t/SVG-Graph-Kit.t
@@ -1,46 +1,19 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
-use Test::More 'no_plan';#tests => 1;
-
-use lib 'lib';
+use Test::More 'no_plan'; #tests => 4;
 use_ok 'SVG::Graph::Kit';
-
-TODO: {
-    local $TODO = "without constructor arguments";
-    my $g = eval { SVG::Graph::Kit->new };
-    warn $@ if $@;
-    isa_ok $g, 'SVG::Graph::Kit';
-}
-
+my $g = eval { SVG::Graph::Kit->new };
+warn $@ if $@;
+isa_ok $g, 'SVG::Graph::Kit', 'Without arguments';
 __END__
 my $i = 0;
-my %args = (
-    width => 600, height => 600, margin => 30,
-    items => [
-        { axis => { 'x_absolute_ticks' => 1, 'y_absolute_ticks' => 1,
-                    'stroke' => 'black', 'stroke-width' => 2 },
-        },
-        { data => [ map { [ $i++, int(rand 50)] }
-            qw(2 3 5 7 11 13 17 19 23 29 31 37 41) ],
-          scatter => { fill => 'white', 'fill-opacity' => 1,
-            stroke => 'blue' },
-          line => { fill => 'yellow', 'fill-opacity' => 0.5,
-            stroke => 'yellow' },
-        },
-        { data => [ map { [int(rand 50), int(rand 50)] } 0 .. 20 ],
-          bar => { fill => 'green', 'fill-opacity' => 0.5,
-            stroke => 'green' },
-        },
-    ],
-);
-my $g = eval { SVG::Graph::Kit->new(%args) };
-print $g->draw;
+my @x = qw(2 3 5 7 11 13 17 19 23 29 31 37 41);
+my $data = [ map { [ ++$i, $_ ] } @x ];
+$i = 0;
+my %args = ( _autoaxis => { m => @x, n => $x[-1], s => @x } );
+$g = eval { SVG::Graph::Kit->new(%args) };
 warn $@ if $@;
-isa_ok $g, 'SVG::Graph::Kit';
-local $/;
-my $svg = 't/SVG-Graph-Kit.svg';
-open SVG, $svg or die "Can't read $svg";
-$svg = <SVG>;
-close SVG;
-is $svg, $g->draw, 'draw';
+isa_ok $g, 'SVG::Graph::Kit', 'With arguments';
+# XXX This is one lame test:
+ok $g->draw, 'Can draw';
